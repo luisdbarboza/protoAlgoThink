3135450986f105863ef5418c59e09a4512726af3
luisdbarboza <luisdavidbn23@gmail.com>
2021-01-23 20:47:38 -0400

    Updates

---
+++ precache-manifest.227a174a2137150b64156957490285be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb5cb5a818e20ec86d557dda5503d70",
+    "url": "/luisdbarboza/protoAlgoThink/topico/index.html"
+  },
+  {
+    "revision": "c9e77558eda94e7e0ddb",
+    "url": "/luisdbarboza/protoAlgoThink/topico/static/js/2.0ef2ef86.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/luisdbarboza/protoAlgoThink/topico/static/js/2.0ef2ef86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "368545faf63e423c6486",
+    "url": "/luisdbarboza/protoAlgoThink/topico/static/js/main.c9c9d409.chunk.js"
+  },
+  {
+    "revision": "32506da6c164dcdb20bf",
+    "url": "/luisdbarboza/protoAlgoThink/topico/static/js/runtime-main.44080750.js"
+  }
+]);
